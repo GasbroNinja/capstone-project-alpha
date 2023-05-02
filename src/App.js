daa585295a80292c69c79da053ff34b6012e48ab
GasbroNinja <gasbroider@live.it>
2023-05-02 18:33:12 +0200

    Initialize Repository Capstone Project Alpha

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
