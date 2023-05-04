a69ce3cee2d5e8d738991804b5ca43d3eb05d433
GasbroNinja <gasbroider@live.it>
2023-05-04 22:39:20 +0200

    Fixare sfondo Zoom-Out, landing page closetobe end

--- src/components/NavbarMain.jsx
+++ src/components/NavbarMain.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { Button, Offcanvas } from 'react-bootstrap';
+import { Button, Container, Offcanvas } from 'react-bootstrap';
+import { ImArrowDown } from "react-icons/im";
 import logoredshift from "../assets/images/LogoRedShift2023.png";
 import "../App.css"
 
@@ -15,50 +16,63 @@ const NavbarMain = ({ ...props }) => {
 
   return (
     <>
-      <Button variant="outline-secondary" onClick={toggleShow} className="me-2">
-        <img src={logoredshift} alt="logo-redshift" className="logoRedshift" />
-      </Button>
-      <Offcanvas
-        className="offcanvasStyle1"
-        show={show}
-        onHide={handleClose}
-        {...props}
+      <Container
+        fluid
+        className="d-flex justify-content-start align-items-center"
       >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title className="pt-5 my-5 settingTextNav backgroundTextNav d-flex flex-column align-items-center ">
-            Redshift Gaming
-          </Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body className=" d-flex flex-column align-items-center justify-content-top">
-          <Button
-            variant="link"
-            className="settingTextNav backgroundTextNav pb-5"
-          >
-            Home
-          </Button>
+        <Button
+          variant="outline-dark"
+          onClick={toggleShow}
+          className=" me-2 d-flex align-items-center justify-content-top px-3"
+        >
+          <h2 className="btnNavOffcanv text-danger fs-2 pe-3 pt-1">RED</h2>
+          <ImArrowDown className="arrowbtnNavOffcanv fs-2"></ImArrowDown>
+        </Button>
+        <Offcanvas
+          className="offcanvasStyle1 overflow-x-hidden"
+          show={show}
+          onHide={handleClose}
+          {...props}
+        >
+          <Offcanvas.Header className="d-flex justify-content-center">
+            <Offcanvas.Title className="w-100 pt-5 my-5 settingTextNav backgroundTextNav textTitleNavOffcanv d-flex flex-column align-items-center justify-content-center ">
+              Redshift Gaming
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body className=" d-flex flex-column align-items-center justify-content-top">
+            <Button
+              variant="link"
+              className="settingTextNav backgroundTextNav pb-5"
+            >
+              Home
+            </Button>
 
-          <Button
-            variant="link"
-            className="settingTextNav backgroundTextNav pb-5"
-          >
-            Tesseramento
-          </Button>
+            <Button
+              variant="link"
+              className="settingTextNav backgroundTextNav pb-5"
+            >
+              Tesseramento
+            </Button>
 
-          <Button
-            variant="link"
-            className="settingTextNav backgroundTextNav pb-5"
-          >
-            Community
-          </Button>
+            <Button
+              variant="link"
+              className="settingTextNav backgroundTextNav pb-5"
+            >
+              Community
+            </Button>
 
-          <Button
-            variant="link"
-            className="settingTextNav backgroundTextNav pb-5"
-          >
-            Eventi
-          </Button>
-        </Offcanvas.Body>
-      </Offcanvas>
+            <Button
+              variant="link"
+              className="settingTextNav backgroundTextNav pb-5"
+            >
+              Eventi
+            </Button>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </Container>
+      <div className="d-flex justify-content-center">
+        <img src={logoredshift} alt="logo-redshift" className="logoRedshift" />
+      </div>
     </>
   );
 };
